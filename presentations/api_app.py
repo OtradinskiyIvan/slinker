7d2f2bf9d414f6aac40d0b7677cb2d88b6ff1b39
OtradinskiyIvan <ivanotradinskij@gmail.com>
2026-01-30 21:59:24 +0300

    added 422 exception and logger

--- presentations/api_app.py
+++ presentations/api_app.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI, HTTPException, Response, status
-from pydantic import BaseModel
-from services.link_service import LinkService
+from idlelib.autocomplete import TRY_A
 
+from fastapi import FastAPI, HTTPException, Response, status, Request
+from pydantic import BaseModel
+from requests import RequestException
+from urllib3.exceptions import RequestError
+
+from services.link_service import LinkService
+import time
+import requests
+from loguru import logger
+
+
+# TODO: link-str -> link-HttpUrl, IsValid(422 Exception)
 
 def create_app() -> FastAPI:
     app = FastAPI()
@@ -15,8 +25,34 @@ def create_app() -> FastAPI:
 
     @app.post("/link")
     def create_link(put_link_request: PutLink) -> PutLink:
-        short_link = short_link_service.create_link(put_link_request.link)
-        return PutLink(link=_service_link_to_real(short_link))
+        inp_link = put_link_request.link
+        url = inp_link if inp_link.startswith(('https://', 'http://')) else 'https://' + inp_link
+
+        try:
+            test_get = requests.get(url)
+            is_valid = True if test_get.status_code < 400 else False
+
+            if is_valid:
+                short_link = short_link_service.create_link(url)
+                return PutLink(link=_service_link_to_real(short_link))
+            else:
+                raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_CONTENT)
+        except RequestException as e:
+            logger.exception("connection via {} url failed. Error: {}", inp_link, e)
+            raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_CONTENT)
+
+
+    @app.middleware("http")
+    async def add_process_time_header(request: Request, call_next) -> Response:
+        t0 = time.time()
+
+        response = await call_next(request)
+        elapsed_ms = round((time.time() - t0) * 1000, 2)
+        response.headers["X-Process-Time"] = str(elapsed_ms)
+        logger.debug("{} {} done in {}ms", request.method, request.url, elapsed_ms)
+
+        return response
+
 
     @app.get("/{link}")
     def get_link(link: str) -> Response:
